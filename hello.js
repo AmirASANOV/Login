69b6df52b88ebc5f3b58377815ab848a5035d5dc
AmirASANOV <genishbekovamir7@gmail.com>
2023-04-22 16:08:21 +0300

    first

---
+++ hello.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('.username')
+const output = document.querySelector('.hello')
+
+document.querySelector('.button').addEventListener('click', function() {
+    let container = document.querySelector('.container')
+    container.style.display = 'none'
+    
+})
